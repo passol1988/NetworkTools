7aec63099f4fb2e0a4ae863a4177523024601ebf
passol1988 <passol@163.com>
2015-09-07 02:56:52 +0800

    Initial Commit

---
+++ NetworkTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkTools
+//
+//  Created by passol on 15/9/7.
+//  Copyright (c) 2015年 passol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkTools
+//
+//  Created by passol on 15/9/7.
+//  Copyright (c) 2015年 passol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
