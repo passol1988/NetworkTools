a75c8c1b4a17dd4f15f6ec8ea038c4a848938152
passol <passol@163.com>
2015-09-07 18:50:08 +0800

    fix

--- NetworkTools/DetectViewController.m
+++ NetworkTools/DetectViewController.m
@@ -45,17 +45,26 @@
     [super viewWillAppear:animated];
     
     NSString *ssid = [[self class] getWifiSSID];
+    NSString *cputype = [[self class] getCPUType];
+    NSString *totalmemory = [NSString stringWithFormat:@"大小%d",[[self class] getTotalMemoryBytes]];
     NSString *memory = [NSString stringWithFormat:@"大小%f",[[self class] getCurrentApplicationUseMemory]];
     NSString *ipadd = [[self class] getIPAddress:YES];
     NSDictionary *ipadd_dict = [[self class] getIPAddresses];
+    NSArray *runningProcesses = [[self class] getRunningProcesses];
 
     NSMutableString *mstr = [NSMutableString new];
     if (ssid) {
         [mstr appendFormat:@"SSID:%@\n", ssid];
     }
+    if (cputype) {
+        [mstr appendFormat:@"cpu类型:%@\n",cputype];
+    }
     
     if (memory) {
         [mstr appendFormat:@"内存大小:%@\n", memory];
+    }
+    if (totalmemory) {
+        [mstr appendFormat:@"总内存大小:%@\n", totalmemory];
     }
     
     if (ipadd) {
@@ -63,7 +72,11 @@
     }
     
     if (ipadd_dict) {
-        [mstr appendFormat:@"地址簇:%@", ipadd_dict];
+        [mstr appendFormat:@"地址簇:%@\n", ipadd_dict];
+    }
+    
+    if (runningProcesses) {
+        [mstr appendFormat:@"运行程序:%@\n", runningProcesses];
     }
     
     self.textView.text = mstr;
@@ -90,6 +103,103 @@
     }
     return nil;
 }
+
++(NSString*)getCPUType
+{
+    NSMutableString *cpu = [[NSMutableString alloc] init];
+    size_t size;
+    cpu_type_t type;
+    cpu_subtype_t subtype;
+    size = sizeof(type);
+    sysctlbyname("hw.cputype", &type, &size, NULL, 0);
+    
+    size = sizeof(subtype);
+    sysctlbyname("hw.cpusubtype", &subtype, &size, NULL, 0);
+    
+    // values for cputype and cpusubtype defined in mach/machine.h
+    if (type == CPU_TYPE_X86)
+    {
+        [cpu appendString:@"x86 "];
+        // check for subtype ...
+        
+    } else if (type == CPU_TYPE_ARM)
+    {
+        [cpu appendString:@"ARM"];
+        [cpu appendFormat:@",Type:%d",subtype];
+    }
+    return cpu;
+    
+}
+
++ (NSUInteger)getTotalMemoryBytes
+{
+    size_t size = sizeof(int);
+    int results;
+    int mib[2] = {CTL_HW, HW_PHYSMEM};
+    sysctl(mib, 2, &results, &size, NULL, 0);
+    return (NSUInteger) results/1024/1024;
+}
+
++ (NSArray *)getRunningProcesses {
+    
+    int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0};
+    size_t miblen = 4;
+    
+    size_t size;
+    int st = sysctl(mib, miblen, NULL, &size, NULL, 0);
+    
+    struct kinfo_proc * process = NULL;
+    struct kinfo_proc * newprocess = NULL;
+    
+    do {
+        
+        size += size / 10;
+        newprocess = realloc(process, size);
+        
+        if (!newprocess){
+            
+            if (process){
+                free(process);
+            }
+            
+            return nil;
+        }
+        
+        process = newprocess;
+        st = sysctl(mib, miblen, process, &size, NULL, 0);
+        
+    } while (st == -1 && errno == ENOMEM);
+    
+    if (st == 0){
+        
+        if (size % sizeof(struct kinfo_proc) == 0){
+            int nprocess = size / sizeof(struct kinfo_proc);
+            
+            if (nprocess){
+                
+                NSMutableArray * array = [[NSMutableArray alloc] init];
+                
+                for (int i = nprocess - 1; i >= 0; i--){
+                    
+                    NSString * processID = [[NSString alloc] initWithFormat:@"%d", process[i].kp_proc.p_pid];
+                    NSString * processName = [[NSString alloc] initWithFormat:@"%s", process[i].kp_proc.p_comm];
+                    
+                    NSDictionary * dict = [[NSDictionary alloc] initWithObjects:[NSArray arrayWithObjects:processID, processName, nil]
+                                                                        forKeys:[NSArray arrayWithObjects:@"ProcessID", @"ProcessName", nil]];
+
+                    [array addObject:dict];
+                }
+                
+                free(process);
+                return array;
+            }
+        }
+    }
+    
+    
+    return nil;
+}
+
 
 +(double)getCurrentApplicationUseMemory
 {
